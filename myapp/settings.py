d71e32d8236d2ef780894ce896ad3960213c05df
AnisurRahman06046 <anisurrahman06046@gmail.com>
2025-01-04 22:24:12 +0600

    updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-
+PORT = os.getenv('PORT', '8000')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
